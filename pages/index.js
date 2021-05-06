7141b73ba85720fce0b600829c681e5d83019e1a
Avutzhan Dautov <dautov92@list.ru>
2021-05-06 15:27:47 +0600

    next js setup and pages

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Index = () => (
+    <div>
+        <h1>SSR testing</h1>
+        <Link href={'/about'}>
+            <button>About</button>
+        </Link>
+
+    </div>
+)
+
+export default Index
